5f3d8be2715b8684e075a10feca5b9fc944c50cf
jiangtao <jt15801656166@gmail.com>
2023-07-20 16:59:40 +0800

    init

--- example/main.tf
+++ example/main.tf
@@ -8,13 +8,6 @@ terraform {
   }
 }
 
-provider "namecheap" {
-  user_name = "haker0032"
-  api_user = "haker0032"
-  api_key = "34a51f53d7294358b213f0f65a7f9da7"
-  client_ip = "180.255.72.50"
-  use_sandbox = false
-}
 
 resource "namecheap_domain_records" "domain-com" {
   domain = "domain.com"
