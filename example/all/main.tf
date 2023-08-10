608f167e6472aa5d195f3f2c21fade7175c821a1
jiangtao <jt15801656166@gmail.com>
2023-08-10 15:41:53 +0800

    modify docs

--- example/all/main.tf
+++ example/all/main.tf
@@ -34,46 +34,6 @@ provider "st-namecheap" {
   client_ip = "180.255.72.50"
   use_sandbox = false
 
-  registrant_firstname = "John"
-  registrant_lastname = "Smith"
-  registrant_address1 = "Street Ave. 666"
-  registrant_city      ="New City"
-  registrant_state_province ="CA"
-  registrant_postal_code    ="90045"
-  registrant_country       ="US"
-  registrant_phone         ="+1.6613102107"
-  registrant_email_address ="john@gmail.com"
-
-  tech_firstname = "John"
-  tech_lastname = "Smith"
-  tech_address1 = "Street Ave. 666"
-  tech_city = "New City"
-  tech_state_province = "CA"
-  tech_postal_code = "90045"
-  tech_country = "US"
-  tech_phone = "+1.6613102107"
-  tech_email_address = "john@gmail.com"
-
-  admin_firstname = "John"
-  admin_lastname = "Smith"
-  admin_address1 = "Street Ave. 666"
-  admin_city = "New City"
-  admin_state_province = "CA"
-  admin_postal_code = "90045"
-  admin_country = "US"
-  admin_phone = "+1.6613102107"
-  admin_email_address = "john@gmail.com"
-
-  aux_billing_firstname = "John"
-  aux_billing_lastname = "Smith"
-  aux_billing_address1 = "Street Ave. 666"
-  aux_billing_city = "New City"
-  aux_billing_state_province = "CA"
-  aux_billing_postal_code = "90045"
-  aux_billing_country = "US"
-  aux_billing_phone = "+1.6613102107"
-  aux_billing_email_address = "john@gmail.com"
-
 }
 
 resource "namecheap_domain" "domain-com" {
