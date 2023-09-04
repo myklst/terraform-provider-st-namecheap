9919811260b0db18398a2dfc8d7bb220fa31f671
jiangtao <jt15801656166@gmail.com>
2023-09-04 09:18:32 +0800

    hide account key and secret

--- example/all/import.tf
+++
@@ -1,7 +0,0 @@
-
-import {
-  to = namecheap_domain.domain-com
-  id = "sige-test11.com"
-}
-
-
--- example/provider/provider.tf
+++ example/provider/provider.tf
@@ -9,9 +9,9 @@ terraform {
 
 
 provider "st-namecheap" {
-  user_name = "haker0032"
-  api_user = "haker0032"
-  api_key = "34a51f53d7294358b213f0f65a7f9da7"
+  user_name = "XXX"
+  api_user = "XXX"
+  api_key = "XXXX"
   client_ip = "180.255.72.50"
   use_sandbox = false
 
